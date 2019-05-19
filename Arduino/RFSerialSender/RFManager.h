3b319123bca37b29bb224788b6ac3298774c5373
Imanol Gomez <yo@imanolgomez.net>
2019-05-19 23:46:04 +0300

    added RFSerialSender

---
+++ Arduino/RFSerialSender/RFManager.h
@@ -0,0 +1,93 @@
+///////////////////////////////////////////////////////////////////
+// Class controlling the RF Communications
+//
+//
+// This code is under A Creative Commons Attribution/Share-Alike License
+// http://creativecommons.org/licenses/by-sa/4.0/
+// 2017, Imanol Gomez
+///////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "Arduino.h"
+#include "FastLED.h"
+#include <SPI.h>
+#include <NRFLite.h>
+
+const static uint8_t RADIO_ID = 1;       // Our radio's id.  The transmitter will send to this id.
+const static uint8_t DESTINATION_RADIO_ID = 0; // Id of the radio we will transmit to.
+const static uint8_t PIN_RADIO_CE = 10;
+const static uint8_t PIN_RADIO_CSN = 9;
+
+
+class RFManager{
+
+  public:
+    
+    RFManager();
+    
+    void setup();
+    void update();
+    void sendData(int id, int instruction);
+    void sendMessage(uint8_t* buf, uint8_t len);
+
+
+  private:
+
+    void setupRF();
+    
+  private:
+
+    NRFLite _radio;
+};
+
+RFManager::RFManager()
+{
+   //Intentionally Left Empty
+}
+
+void RFManager::setup()
+{
+    this->setupRF();
+}
+
+void RFManager::setupRF()
+{
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+    {
+        Serial.println("RFManager::setupRF -> Cannot communicate with radio");
+        while (1); // Wait here forever.
+    }
+
+    Serial.println("RFManager::setupRF ->Successfully connected");
+    
+    /*
+    By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
+    Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+    You can assign a different bitrate and channel as shown below.
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+    _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
+    */
+}
+
+
+void RFManager::update()
+{
+ 
+}
+
+void RFManager::sendMessage(uint8_t* buf, uint8_t len)
+{
+
+  if (_radio.send(DESTINATION_RADIO_ID, buf, len)) 
+    {
+        ///
+    }
+    else
+    {
+      // Serial.println("RFManager::sendData -> Could not send data ");
+    }
+}
+
+
+   
---
+++ Arduino/RFSerialSender/RFSerialSender.ino
@@ -0,0 +1,42 @@
+
+ /*
+  
+  RF Serial Sender 
+     
+ Description:
+ * Firmware sending Serial changes using the an RF module
+ Software:
+ * RFLite
+ 
+ Hardware:
+* Arduino Nano
+* NRF24L01+ module
+
+
+ created 19 May 2019
+ This code is under A Creative Commons Attribution/Share-Alike License http://creativecommons.org/licenses/by-sa/4.0/
+   (2019) by Imanol Gomez
+
+ */
+ 
+
+#include "RFManager.h"
+#include "SerialManager.h"
+
+
+RFManager rfManager;
+SerialManager serialManager(&rfManager);
+
+
+void setup() 
+{
+    serialManager.setup();
+    rfManager.setup();
+}
+
+void loop() 
+{
+    rfManager.update();
+    serialManager.update();
+ 
+}
